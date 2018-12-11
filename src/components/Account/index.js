4f8f37dcd5ca090ba3fe0d298080757dd9e26c05
David Simon <david_simon@epam.com>
2018-12-11 19:02:32 +0100

    added protection to several pages (authorization module)

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 import { PasswordForgotForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+import { AuthUserContext, withAuthorization } from '../Session';
 
 const AccountPage = () => (
-    <div>
-        <h1>Account</h1>
-        <PasswordForgotForm />
-        <PasswordChangeForm />
-    </div>
+    <AuthUserContext.Consumer>{authUser => (
+        <div>
+            <h1>Account: {authUser.email}</h1>
+            <PasswordForgotForm />
+            <PasswordChangeForm />
+        </div>
+    )}</AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
