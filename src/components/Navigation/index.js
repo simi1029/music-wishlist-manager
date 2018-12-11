aa242336d8b6c3a1eaa31e14309d50fa6d8aaf5a
David Simon <david_simon@epam.com>
2018-12-11 16:05:53 +0100

    adding SignOut feature

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
+import SignOutButton from '../SignOut';
 
 const Navigation = () => (
     <div>
@@ -20,6 +21,9 @@ const Navigation = () => (
             </li>
             <li>
                 <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+            <li>
+                <SignOutButton />
             </li>
         </ul>
     </div>
