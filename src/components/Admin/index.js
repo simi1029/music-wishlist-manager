ac1918cc8860b70f9e2018af35018a210cc603d4
David Simon <sim.david90@gmail.com>
2018-12-12 01:11:16 +0100

    added Firebase DB access, Admin page restriction

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { compose } from 'recompose';
 
-const Admin = () => (
+import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+
+const AdminPage = () => (
     <div>
-        <h1>Admin</h1>
+        <h1>Admin Page</h1>
+        <AdminView />
     </div>
 );
 
-export default Admin;
+class AdminPageBase extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            users: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+
+        this.props.firebase.users().on('value', snapshot => {
+            const usersObject = snapshot.val();
+
+            const usersList = Object.keys(usersObject).map(key => ({
+                ...usersObject[key],
+                uid: key,
+            }));
+
+            this.setState({
+                users: usersList,
+                loading: false,
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.props.firebase.users().off();
+    }
+
+    render() {
+        const { users, loading } = this.state;
+
+        return (
+            <div>
+                {loading && <div>Loading ...</div>}
+
+                <UserList users={users} />
+            </div>
+        );
+    }
+}
+
+const UserList = ({ users }) => (
+    <ul>
+        {users.map(user => (
+            <li key={user.uid}>
+                <span>
+                    <strong>ID:</strong> {user.uid}
+                </span>
+                <span>
+                    <strong>E-Mail:</strong> {user.email}
+                </span>
+                <span>
+                    <strong>Username:</strong> {user.username}
+                </span>
+            </li>
+        ))}
+    </ul>
+);
+
+const condition = authUser => !!authUser;
+
+const AdminView = compose(
+    withAuthorization(condition),
+    withFirebase,
+)(AdminPageBase);
+
+export default AdminPage;
+
+export { AdminView };
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -30,12 +30,19 @@ class SignUpFormBase extends Component {
     onSubmit = event => {
         const { username, email, passwordOne } = this.state;
 
-        this.props.firebase.doCreateUserWithEmailAndPassword(email, passwordOne).then(authUser => {
-            this.setState({ ...INITIAL_STATE });
-            this.props.history.push(ROUTES.HOME);
-        }).catch((error) => {
-            this.setState({ error });
-        });
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                return this.props.firebase
+                    .user(authUser.user.uid)
+                    .set({username, email, });
+            })
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
+            }).catch((error) => {
+                this.setState({ error });
+            });
 
     event.preventDefault();
     }
